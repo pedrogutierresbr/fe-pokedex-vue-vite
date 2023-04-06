09ba6a0e10fe3721b4827ec14688c495a3bf8627
Pedro Gutierres <pedro-gabriel1@hotmail.com>
2023-04-06 15:22:29 -0300

    created CardPokemonSelected component and selectedPokemon method

---
+++ src/components/CardPokemonSelected.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+const pokemon = defineProps(["name", "xp", "height", "image"])
+
+</script>
+
+<template>
+    <div class="card cardSelected">
+        <img :src="pokemon.image" class="card-img-top pt-2" :alt="pokemon.name" height="250">
+        <div class="card-body">
+            <h5 class="pokemonName card-title text-center">{{ pokemon.name }}</h5>
+            <hr>
+            <div class="row">
+                <section class="col">
+                    <strong>XP: </strong>
+                    <span>{{ pokemon.xp }}</span>
+                </section>
+
+                <section class="col">
+                    <strong>Altura: </strong>
+                    <span>{{ pokemon.height }}</span>
+                </section>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.cardSelected {
+    height: 450px;
+    background: rgb(72, 63, 251);
+    background: radial-gradient(circle, rgba(72, 63, 251, 0.5) 0%, rgba(70, 252, 244, 0.3) 100%);
+}
+
+.pokemonName {
+    text-transform: capitalize;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { onMounted, reactive, ref, computed } from 'vue';
 import ListPokemons from '../components/ListPokemons.vue'
+import CardPokemonSelected from '../components/CardPokemonSelected.vue'
 
 let urlBaseSVG = ref('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/')
 let pokemons = reactive(ref())
 let searchPokemonField = ref("")
+let pokemonSelected = reactive(ref(""))
 
 onMounted(() => {
   fetch('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
@@ -19,6 +21,12 @@ const pokemonsFiltered = computed(() => {
   return pokemons.value
 })
 
+const selectPokemon = async (pokemon) => {
+  await fetch(pokemon.url)
+    .then(res => res.json())
+    .then(res => pokemonSelected.value = res)
+}
+
 </script>
 
 <template>
@@ -27,19 +35,12 @@ const pokemonsFiltered = computed(() => {
       <div class="row mt-4">
 
         <div class="col-sm-12 col-md-6">
-          <div class="card" style="width: 18rem;">
-            <img src="https://assets.pokemon.com/assets/cms2/img/misc/countries/pt/country_detail_pokemon.png"
-              class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                content.</p>
-            </div>
-          </div>
+          <CardPokemonSelected :name="pokemonSelected?.name" :xp="pokemonSelected.base_experience"
+            :height="pokemonSelected.height" :image="pokemonSelected?.sprites?.other.dream_world.front_default" />
         </div>
 
         <div class="col-sm-12 col-md-6">
-          <div class="card">
+          <div class="card cardList">
             <div class="card-body row">
 
               <div class="mb-3">
@@ -48,8 +49,8 @@ const pokemonsFiltered = computed(() => {
                   placeholder="Pesquisar">
               </div>
 
-              <ListPokemons v-for="pokemon in pokemonsFiltered" :key="pokemon.name" :name="pokemon.name"
-                :urlBaseSVG="urlBaseSVG + pokemon.url.split('/')[6] + '.svg'" />
+              <ListPokemons @click="selectPokemon(pokemon)" v-for="pokemon in pokemonsFiltered" :key="pokemon.name"
+                :name="pokemon.name" :urlBaseSVG="urlBaseSVG + pokemon.url.split('/')[6] + '.svg'" />
             </div>
           </div>
         </div>
@@ -58,3 +59,11 @@ const pokemonsFiltered = computed(() => {
     </div>
   </main>
 </template>
+
+<style scoped>
+.cardList {
+  max-height: 450px;
+  overflow-y: scroll;
+  overflow-x: hidden;
+}
+</style>
