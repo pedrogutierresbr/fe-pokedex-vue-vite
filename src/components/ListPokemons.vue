7aa91b5e8a737a8fd03d7a8f758d36d9c1e6cc31
Pedro Gutierres <pedro-gabriel1@hotmail.com>
2023-04-06 12:33:48 -0300

    added gradient on card background

--- src/components/ListPokemons.vue
+++ src/components/ListPokemons.vue
@@ -6,9 +6,27 @@ const pokemon = defineProps(['name', 'urlBaseSVG'])
 
 <template>
     <div class="col-4">
-        <div class="card p-2 mb-3">
+        <div class="card p-2 mb-3 cardListPokemons">
             <p class="text-center">{{ pokemon.name }}</p>
             <img :src="urlBaseSVG" class="card-img-top" :alt="pokemon.name" height="80" />
         </div>
     </div>
 </template>
+
+<style scoped>
+.cardListPokemons {
+    background: rgb(72, 63, 251);
+    background: radial-gradient(circle, rgba(72, 63, 251, 0.5) 0%, rgba(70, 252, 244, 0.3) 100%);
+    cursor: pointer;
+}
+
+.cardListPokemons:hover {
+    background: rgb(72, 63, 251);
+    background: radial-gradient(circle, rgba(72, 63, 251, 0.6) 0%, rgba(70, 252, 244, 0.4) 100%);
+    cursor: pointer;
+}
+
+.cardListPokemons img:hover {
+    padding: .1rem;
+}
+</style>
